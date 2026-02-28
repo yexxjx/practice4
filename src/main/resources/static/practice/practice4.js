ce94d2e4098c6bbe3743feb1ba8158c74b98eb80
yexxjx <chenstyle02@gmail.com>
2026-02-28 17:38:24 +0900

    0228 JS 기능 완료

--- src/main/resources/static/practice/practice4.js
+++ src/main/resources/static/practice/practice4.js
@@ -23,7 +23,41 @@ const deptFindAll = async () => {
                 </span>
             </div>`;
     }
-
     deptContainer.innerHTML = html;
 }
 deptFindAll();
+
+const deptCreate=async()=>{
+    const deptnameInput=document.querySelector("#deptname");
+    const deptname=deptnameInput.value;
+    const obj={"deptname":deptname}
+    const response=await axios.post("/practice4/dept", obj);
+    const data=response.data;
+    if(data==true){
+        alert("등록 성공");
+        deptnameInput.value="";
+        deptFindAll();
+    } else{alert("등록 실패");}
+}
+
+const deptDelete=async(dno)=>{
+    const response=await axios.delete(`/practice4/dept?dno=${dno}`);
+    const data=response.data;
+    if(data==true){
+        alert("삭제 성공");
+        deptFindAll();
+    } else{alert("삭제 실패");}
+}
+
+const deptUpdate=async(dno)=>{
+    const deptname=prompt("수정할 부서명");
+    const obj={dno, deptname}
+    const response=await axios.put("/practice4/dept", obj);
+    const data=response.data;
+    if(data==true){
+        alert("수정 성공");
+        deptFindAll();
+    } else{alert("수정 실패");}
+}
+
+// DB foreign key 문장 뒤에 on delete cascade 넣기
